d50082e8021949bf410a27b407a220b8090f4756
JianBenedict <100068737+JianBenedict@users.noreply.github.com>
2022-11-16 13:48:00 +0800

    Add files via upload

---
+++ LT4/src/Animals.java
@@ -0,0 +1,33 @@
+class Animals{
+    void facts(){System.out.println("RANDOM ANIMAL FACTS");}
+}
+class Lion extends Animals{
+    void facts(){
+        System.out.println("");
+        System.out.println("LION FACTS");
+        System.out.println("1.Lions run at a speed of up to 81kmph.");
+        System.out.println("2.A female lion needs 5kg of meat a day. A male needs 7kg or more a day.");
+        System.out.println("3.An adult male’s roar can be heard up to 8km away.");
+        System.out.println("");
+    }
+}
+class Tiger extends Animals{
+    void facts(){
+        System.out.println("");
+        System.out.println("TIGER FACTS");
+        System.out.println("1.Tiger cubs are born blind and only half of the cubs survive :(");
+        System.out.println("2.A group of Tigers are called an ambush or streak");
+        System.out.println("3.Tigers can sprint at over 60 kilometre/hour");
+        System.out.println("");
+    }
+}
+class Elephant extends Animals{
+    void facts(){
+        System.out.println("");
+        System.out.println("ELEPHANT FACTS");
+        System.out.println("1.An elephant's trunk weighs 400 pounds, but it can pick up things as small as a single grain of rice.");
+        System.out.println("2.Elephants can recognize themselves in the mirror. They join humans, apes, and dolphins as the only animals with self awareness.");
+        System.out.println("3.Elephants are pregnant for 22 months — it's the longest gestation period of any mammal.");
+        System.out.println("");
+    }
+}
---
+++ LT4/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Animals A;
+        A=new Lion();
+        A.facts();
+        /////////////////////
+        A=new Tiger();
+        A.facts();
+        ////////////////////
+        A=new Elephant();
+        A.facts();
+        ///////////////////
+    }
+}
+
+
